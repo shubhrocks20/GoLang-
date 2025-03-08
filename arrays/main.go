8829a1b6be47251e860227d4124faf76167407f6
Shubham Kumar <Shubhamkumar2056@gmail.com>
2025-03-08 17:51:35 +0530

    learnt Arrays

---
+++ arrays/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// numbered sequence of specific length
+func main() {
+	// zero values
+	var nums [4]int
+	// array length
+	fmt.Println(len(nums))
+
+	nums[0] = 1
+	fmt.Println(nums)
+
+	var newArray [4]string
+	newArray[1] = "shubham"
+	fmt.Println(newArray)
+	// to declare in single line
+	numbers := [3]int{1,2,3}
+	fmt.Println(numbers)
+
+	// 2d arrays
+
+	numbers2 := [2][2]int{{1,2},{3,4}}
+	fmt.Println(numbers2)
+
+	// - fixed size, that is predictable
+	// - Memory optimization
+	// - Constant time access
+
+}
